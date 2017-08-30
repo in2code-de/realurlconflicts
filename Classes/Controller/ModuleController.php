c0805cf8349c56a3eff6a7db4783b40c529a9ca8
Alex Kellner <alexander.kellner@in2code.de>
2017-08-30 14:56:56 +0200

    [TASK] Add a basic module

---
+++ Classes/Controller/ModuleController.php
@@ -0,0 +1,18 @@
+<?php
+namespace In2code\Realurlconflicts\Controller;
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+/**
+ * Class ModuleController
+ */
+class ModuleController extends ActionController
+{
+
+    /**
+     * @return void
+     */
+    public function conflictsAction()
+    {
+    }
+}
---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ipandlanguageredirect".
+ *
+ * Auto generated 04-07-2013 17:03
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'realurlconflicts',
+    'description' => 'TYPO3 extension show realurl conflicts in an own backend module.',
+    'category' => 'module',
+    'shy' => 0,
+    'version' => '1.0.0',
+    'dependencies' => 'cms',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 1,
+    'lockType' => '',
+    'author' => 'Alex Kellner',
+    'author_email' => 'alexander.kellner@in2code.de',
+    'author_company' => 'in2code.de',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.99.99',
+            'php' => '7.0.0-7.99.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    '_md5_values_when_last_written' => '',
+];

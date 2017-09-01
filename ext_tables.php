c17d125fa63b058590d4f05dceaaf197ba92d942
Alex Kellner <alexander.kellner@in2code.de>
2017-09-01 11:14:58 +0200

    [BUGFIX] German localization for backend module was wrong in XLIFF

--- ext_tables.php
+++ ext_tables.php
@@ -3,23 +3,25 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(function () {
+call_user_func(
+    function () {
 
-    /**
-     * Include Backend Module
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'In2code.realurlconflicts',
-        'web',
-        'm1',
-        '',
-        array(
-            'Module' => 'conflicts,deleteCache,deleteCacheWithDeletedPages'
-        ),
-        array(
-            'access' => 'user,group',
-            'icon' => 'EXT:realurlconflicts/Resources/Public/Icons/Module.svg',
-            'labels' => 'LLL:EXT:realurlconflicts/Resources/Private/Language/locallang_mod.xlf',
-        )
-    );
-});
+        /**
+         * Include Backend Module
+         */
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'In2code.realurlconflicts',
+            'web',
+            'm1',
+            '',
+            [
+                'Module' => 'conflicts,deleteCache,deleteCacheWithDeletedPages'
+            ],
+            [
+                'access' => 'user,group',
+                'icon' => 'EXT:realurlconflicts/Resources/Public/Icons/Module.svg',
+                'labels' => 'LLL:EXT:realurlconflicts/Resources/Private/Language/locallang_mod.xlf',
+            ]
+        );
+    }
+);

d9f9f09bb2a8b54098ef98b2702a2c5b2305becf
Alex Kellner <alexander.kellner@in2code.de>
2017-09-01 12:06:52 +0200

    [TASK] Load backend module only if realurl is enabled

--- ext_tables.php
+++ ext_tables.php
@@ -5,23 +5,30 @@ if (!defined('TYPO3_MODE')) {
 
 call_user_func(
     function () {
-
-        /**
-         * Include Backend Module
-         */
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-            'In2code.realurlconflicts',
-            'web',
-            'm1',
-            '',
-            [
-                'Module' => 'conflicts,deleteCache,deleteCacheWithDeletedPages'
-            ],
-            [
-                'access' => 'user,group',
-                'icon' => 'EXT:realurlconflicts/Resources/Public/Icons/Module.svg',
-                'labels' => 'LLL:EXT:realurlconflicts/Resources/Private/Language/locallang_mod.xlf',
-            ]
-        );
+        if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
+            /**
+             * Include Backend Module
+             */
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'In2code.realurlconflicts',
+                'web',
+                'm1',
+                '',
+                [
+                    'Module' => 'conflicts,deleteCache,deleteCacheWithDeletedPages'
+                ],
+                [
+                    'access' => 'user,group',
+                    'icon' => 'EXT:realurlconflicts/Resources/Public/Icons/Module.svg',
+                    'labels' => 'LLL:EXT:realurlconflicts/Resources/Private/Language/locallang_mod.xlf',
+                ]
+            );
+        } else {
+            \TYPO3\CMS\Core\Utility\GeneralUtility::sysLog(
+                'Extension realurl not loaded - Module for realurlconflicts will not be included!',
+                'realurlconflicts',
+                \TYPO3\CMS\Core\Utility\GeneralUtility::SYSLOG_SEVERITY_WARNING
+            );
+        }
     }
 );
